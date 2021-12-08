8a95f518a3c3bc67a3a737a73a486aa891aa5b7f
tazo90 <tazo90@gmail.com>
2021-12-08 23:14:06 +0100

    Basic app skeleton

---
+++ src/pages/_app.tsx
@@ -0,0 +1,54 @@
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
+import { Hydrate } from 'react-query/hydration';
+import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { AnimatePresence } from "framer-motion";
+
+// Load fonts
+import '../styles/globals.css'
+import '@fontsource/open-sans';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+import '@fontsource/satisfy';
+
+// Import css
+import '@styles/tailwind.css';
+
+function handleExitComplete() {
+	if (typeof window !== "undefined") {
+		window.scrollTo({ top: 0 });
+	}
+}
+
+function Noop({ children }: any) {
+  return <>{children}</>;
+}
+
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const Layout = (Component as any).Layout || Noop;
+  const queryClientRef = useRef();
+  
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout pageProps={pageProps}>
+            <Component {...pageProps} key={router.route} />  
+            <ToastContainer />      
+          </Layout>
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </AnimatePresence>
+  );
+}
+
+export default App
---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+} from 'next/document';
+
+export default class CustomDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        return await Document.getInitialProps(ctx);
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
