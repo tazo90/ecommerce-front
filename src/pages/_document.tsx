fd50d94cfb2ed04e8fb942e20622dd9c91f8c442
tazo90 <tazo90@gmail.com>
2021-12-08 23:15:46 +0100

    Fix indent

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,25 @@
 import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
 } from 'next/document';
 
 export default class CustomDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        return await Document.getInitialProps(ctx);
-    }
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
