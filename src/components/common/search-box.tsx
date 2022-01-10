faa1692ca0c939c38564a782ae867825a6a70cc6
tazo90 <tazo90@gmail.com>
2022-01-10 01:01:33 +0100

    Search box with more options

--- src/components/common/search-box.tsx
+++ src/components/common/search-box.tsx
@@ -1,32 +1,35 @@
-import React, { forwardRef } from 'react';
-import { SearchIcon } from '@components/icons';
+import React, { forwardRef } from "react";
+import { SearchIcon } from "@components/icons";
 import { IoCloseOutline } from "react-icons/io5";
 
 interface SearchProps {
   className?: string;
+  height?: string;
   onSubmit: (e: React.SyntheticEvent) => void;
   onClear: (e: React.SyntheticEvent) => void;
   onChange: (e: React.FormEvent<HTMLInputElement>) => void;
   name: string;
   value: string;
-};
+}
 
 const SearchBox = forwardRef<HTMLInputElement, SearchProps>(
-	({ className, onSubmit, onClear, ...rest }, ref) => {
+  ({ className, onSubmit, onClear, height = "12", ...rest }, ref) => {
     return (
       <form
-        className="relative pe-12 md:pe-14 bg-white overflow-hidden rounded-md w-full"
+        className={`relative pe-12 md:pe-14 bg-white overflow-hidden rounded-md w-full ${className}`}
         noValidate
         role="search"
         onSubmit={onSubmit}
       >
         <label htmlFor="search" className="flex items-center py-0.5">
           <span className="w-12 md:w-14 h-full flex flex-shrink-0 justify-center items-center cursor-pointer focus:outline-none">
-            <SearchIcon color="text-heading" className="w-4 h-4" />        
+            <SearchIcon color="text-heading" className="w-4 h-4" />
           </span>
-          <input 
+          <input
             id="search"
-            className="text-heading outline-none w-full h-12 lg:h-14 placeholder-gray-400 text-sm lg:text-base"
+            className={`text-heading outline-none w-full h-${height} lg:h-${
+              height + 2
+            } placeholder-gray-400 text-sm lg:text-base`}
             placeholder="search"
             aria-label="Search"
             autoComplete="off"
@@ -42,7 +45,7 @@ const SearchBox = forwardRef<HTMLInputElement, SearchProps>(
           <IoCloseOutline className="w-6 h-6" />
         </button>
       </form>
-    )
+    );
   }
 );
 
